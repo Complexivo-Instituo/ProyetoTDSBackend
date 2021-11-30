69a292f5f8f663bb613378e2fec90d395d4f105b
@henry.brito.est <henry.brito.est@tecazuay.edu.ec>
2021-11-30 11:54:58 -0500

    Nuevos modelos
    Modelos creados con el controlador/service/repository

---
+++ src/main/java/com/ProyectoTDSBackend/controller/AsignaturaController.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ProyectoTDSBackend.controller;
+
+import com.ProyectoTDSBackend.dto.Mensaje;
+import com.ProyectoTDSBackend.models.Convocatoria;
+import com.ProyectoTDSBackend.service.AsignaturaService;
+import com.ProyectoTDSBackend.service.ConvocatoriaService;
+import io.swagger.annotations.ApiOperation;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class AsignaturaController {
+
+    @Autowired
+    AsignaturaService asignaturaService;
+
+//Listar todas las empresas
+    @ApiOperation("Listado de las asignaturas")
+    @CrossOrigin({"*"})
+    @GetMapping("/asignaturas")
+    public List<Asignatura> getAllAsignaturas() {
+        return asignaturaService.getlListaAsignaturas();
+    }
+
+    // 
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody Asignatura asignatura1) {
+        if (StringUtils.isBlank(asignatura1.getNombreasignatura())) {
+            return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+        }
+        Asignatura asignatura = new Asignatura(asignatura1.getNombreasignatura(), asignatura1.getPromedio());
+
+        asignaturaService.save(asignatura);
+
+        return new ResponseEntity(new Mensaje("Convocatoria creada"), HttpStatus.OK);
+    }
+}
--- src/main/java/com/ProyectoTDSBackend/controller/ConvocatoriaController.java
+++ src/main/java/com/ProyectoTDSBackend/controller/ConvocatoriaController.java
@@ -8,8 +8,11 @@ package com.ProyectoTDSBackend.controller;
 import com.ProyectoTDSBackend.dto.Mensaje;
 import com.ProyectoTDSBackend.models.Actividad;
 import com.ProyectoTDSBackend.models.Convocatoria;
+import com.ProyectoTDSBackend.models.Empresa;
 import com.ProyectoTDSBackend.service.ConvocatoriaService;
+import com.ProyectoTDSBackend.util.GenericResponse;
 import io.swagger.annotations.ApiOperation;
+import java.util.Date;
 import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +20,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -30,21 +35,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/convocatoria")
 @CrossOrigin({"*"})
 public class ConvocatoriaController {
-    
+
     @Autowired
-    ConvocatoriaService actividadService;
+    ConvocatoriaService convocatoriaService;
 
 //Listar todas las empresas
     @ApiOperation("Listado de las convocatorias")
     @CrossOrigin({"*"})
     @GetMapping("/convocatorias")
     public List<Convocatoria> getAllConvocatorias() {
-        return actividadService.getlListaActividades();
+        return convocatoriaService.getlListaActividades();
     }
-    
-    
+
     // 
-     @PostMapping("/create")
+    @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody Convocatoria convocatoria1) {
         if (StringUtils.isBlank(convocatoria1.getDescripcion_convocatoria())) {
             return new ResponseEntity(new Mensaje("La descripcion es obligatoria"), HttpStatus.BAD_REQUEST);
@@ -52,13 +56,37 @@ public class ConvocatoriaController {
         if (StringUtils.isBlank(convocatoria1.getFecha_inicio().toString())) {
             return new ResponseEntity(new Mensaje("La fecha de inicio es obligatoria"), HttpStatus.BAD_REQUEST);
         }
-         if (StringUtils.isBlank(convocatoria1.getFecha_inicio().toString())) {
+        if (StringUtils.isBlank(convocatoria1.getFecha_inicio().toString())) {
             return new ResponseEntity(new Mensaje("La fecha fin es obligatoria"), HttpStatus.BAD_REQUEST);
         }
         Convocatoria convocatoria = new Convocatoria(convocatoria1.getDescripcion_convocatoria(), convocatoria1.getFecha_inicio(), convocatoria1.getFecha_fin());
-        actividadService.save(convocatoria);
+        convocatoria.setEstado(1);
+        convocatoriaService.save(convocatoria);
 
         return new ResponseEntity(new Mensaje("Convocatoria creada"), HttpStatus.OK);
+    }
+
+    @ApiOperation("Actualizar convocatoria")
+    @CrossOrigin
+    @PostMapping("/actualizarConvocatoria")
+    ResponseEntity<GenericResponse<Object>> putConvocatoria(
+            @RequestParam(value = "idconvocatoria") Long idconvocatoria,
+            @RequestParam(value = "descripcion_convocatoria") String descripcion,
+            @RequestParam(value = "fecha_inicio") Date fecha_inicio,
+            @RequestParam(value = "fecha_fin") Date fecha_fin) {
+        return new ResponseEntity<GenericResponse<Object>>(
+                convocatoriaService.putActividad(idconvocatoria, descripcion, fecha_inicio, fecha_fin),
+                HttpStatus.OK);
+    }
+    
+    @ApiOperation("Eliminado logico de convocatoria")
+    @CrossOrigin({"*"})
+    @PatchMapping("/deleteConvocatoria/{idconvocatoria}")
+    public ResponseEntity<?> deleteConvocatoria(@RequestParam(value = "idconvocatoria") Long idconvocatoria) {
+        Convocatoria convocatoria = convocatoriaService.getOne(idconvocatoria).get();
+        convocatoria.setEstado(0);
+        convocatoriaService.save(convocatoria);
+        return new ResponseEntity(new Mensaje("Convocatoria eliminada"), HttpStatus.OK);
     }
 
 }
