77183d15ee0b5c6afc69572264ca9f81b9375ad2
@henry.brito.est <henry.brito.est@tecazuay.edu.ec>
2021-11-30 23:20:21 -0500

    Nuevo rol
    nuevo rol agregado

--- src/main/java/com/ProyectoTDSBackend/util/CreateRoles.java
+++ src/main/java/com/ProyectoTDSBackend/util/CreateRoles.java
@@ -24,17 +24,20 @@ public class CreateRoles implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Rol rolAdmin = new Rol(RolNombre.ROLE_ADMIN);
-        Rol roleEstudiante = new Rol(RolNombre.ROLE_ESTU);
-        Rol rolAcademico = new Rol(RolNombre.ROLE_ACADEMICO);
-        Rol rolCordinador= new Rol(RolNombre.ROLE_COORDINA);
-        Rol rolEmpresa= new Rol(RolNombre.ROLE_EMPRESA);
-        Rol rolVincula = new Rol(RolNombre.ROLE_VICULA);
-        rolService.save(rolAdmin);
-        rolService.save(roleEstudiante);
-        rolService.save(rolAcademico);
-        rolService.save(rolCordinador);
-        rolService.save(rolEmpresa);
-        rolService.save(rolVincula);
+//        Rol rolAdmin = new Rol(RolNombre.ROLE_ADMIN);
+//        Rol roleEstudiante = new Rol(RolNombre.ROLE_ESTU);
+//        Rol rolAcademico = new Rol(RolNombre.ROLE_ACADEMICO);
+//        Rol rolCordinador= new Rol(RolNombre.ROLE_COORDINA);
+//        Rol rolEmpresa= new Rol(RolNombre.ROLE_EMPRESA);
+//        Rol rolVincula = new Rol(RolNombre.ROLE_VICULA);
+
+//      Rol rolResponsable = new Rol(RolNombre.ROLE_RESPONSA);
+//        rolService.save(rolAdmin);
+//        rolService.save(roleEstudiante);
+//        rolService.save(rolAcademico);
+//        rolService.save(rolCordinador);
+//        rolService.save(rolEmpresa);
+//        rolService.save(rolVincula);
+//        rolService.save(rolResponsable);
     }
 }
